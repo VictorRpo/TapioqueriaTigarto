546f12ee890c80e1684d8a67af0ee28e4d4cac93
VictorRpo <victora.agudelo@udea.edu.co>
2021-03-29 22:19:16 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+int main() {
+	//solicitud de los platos
+	//validar que sean 2,3 o 4
+	//si es 1 solo plato, solicitar al cliente que pida mas platos
+	//si es mayor de 4, solicitar que divida el pedido en varios pedidos
+	//validar existencia de los platos en el vector que contiene cantidad de platos disponibles
+	//se envia como argumento un archivo con el formato _____
+	//validar primera linea (4 enteros: 1. indica la cantidad de platos pedidos, 2. # de platos de 2, 3. # platos de 3, 4. # platos 4
+	//describir cada plato
+	//validar que el # de lineas del archivo == al primer # de la primer linea
+	// validar que la suma de los elementos n>1 son igual a n(0)
+	//validar que cada linea de platos corresponda el numero (cantidad) con la cantidad escrita de ingredientes
+	//validar que cada ingrediente sea alfanumerico, minuscula, no mayor a 20 caracteres y no caracteres especiales
+	//los ingredientes son separados por _
+	//controlar caracter nueva linea al final de cada linea de codigo
+	//
+}
